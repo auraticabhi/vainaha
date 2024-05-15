d9b2467ba5e493b407051ddd2d3f19e186cd5e95
auraticabhi <abhijeetgupta989@gmail.com>
2024-05-15 23:31:52 +0530

    ui changes

--- app/events/components/sidebar.tsx
+++ app/events/components/sidebar.tsx
@@ -59,7 +59,7 @@ export function Sidebar({ className, playlists, selectChange, currentC }: Sideba
     <div className={cn(" dark:bg-[#262626] shadow-[0px_0px_0px_1px_rgba(8,112,184,0.06),0px_1px_1px_-0.5px_rgba(8,112,184,0.06),0px_3px_3px_-1.5px_rgba(8,112,184,0.06),_0px_6px_6px_-3px_rgba(8,112,184,0.06),0px_12px_12px_-6px_rgba(8,112,184,0.06),0px_24px_24px_-12px_rgba(8,112,184,0.06)]", className)}>
       <div className="space-y-4 pt-4 pb-2">
         <div className="px-4 py-2">
-          <h2 className="mb-2 px-4 text-lg font-[500] tracking-tight">
+          <h2 className="mb-2 px-4 text-[20px] font-[500] tracking-tight">
             Discover
           </h2>
           <ScrollArea className="h-[320px] px-1">
@@ -116,7 +116,7 @@ export function Sidebar({ className, playlists, selectChange, currentC }: Sideba
     <div className={cn(" dark:bg-[#262626] mt-[14px] shadow-[0px_0px_0px_1px_rgba(8,112,184,0.06),0px_1px_1px_-0.5px_rgba(8,112,184,0.06),0px_3px_3px_-1.5px_rgba(8,112,184,0.06),_0px_6px_6px_-3px_rgba(8,112,184,0.06),0px_12px_12px_-6px_rgba(8,112,184,0.06),0px_24px_24px_-12px_rgba(8,112,184,0.06)]", className)}>
     <div className="space-y-4 pt-4 pb-2">
     <div className="py-1 mt-1">
-          <h2 className="relative px-7 text-lg tracking-tight font-[500]">
+          <h2 className="relative px-7 text-[20px] tracking-tight font-[500]">
             Playlists
           </h2>
           <ScrollArea className="h-[150px] px-1">
